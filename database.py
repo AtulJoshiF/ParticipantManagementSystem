57e1d48ae802a75f9538e33721a50c10891c4c44
AtulJoshiF <atuljoshif@outlook.com>
2023-06-16 12:16:05 +0530

    Database url updated
    The database url has been updated   from local database to elephantSQL service.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/ParticipantManagementSystem'
+SQL_ALCHEMY_DATABASE_URL = 'postgresql://btfmmyqn:HLZklAMpjQnHH2--N2Nj1U-11oTT3L1E@mahmud.db.elephantsql.com/btfmmyqn'
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
